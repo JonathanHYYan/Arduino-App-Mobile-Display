22002ec2bff8c9159954bbdff5ffcb9a437808bf
Jonathan Yan <jonathanyan@Jonathans-MacBook-Pro.local>
2023-06-07 13:45:36 +0100

    [SDCWA-3] Added Page navigation with react-router-dom

--- src/Components/PhoneModel.js
+++ src/Components/PhoneModel.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { MobileTemplate } from "./PhoneModelStyles";
 import SignIn from "./SignIn/SignIn";
 import MainPage from "./Main/MainPage";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Template = () => {
-const phoneList = [
+  const phoneList = [
     { name: "iPhone 12", height: 146.7, width: 71.5 },
     { name: "Pixel 5", height: 144.7, width: 70.4 },
     { name: "Samsung Galaxy Note II", height: 151.1, width: 80.5 },
-    ];
+  ];
   const [selectedPhone, setSelectedPhone] = useState(0);
 
   const handleChange = (e) => {
@@ -26,20 +26,23 @@ const phoneList = [
   });
 
   const phoneStyle = {
-    width: phoneList[selectedPhone].width*4,
-    height: phoneList[selectedPhone].height*4,
+    width: phoneList[selectedPhone].width * 4,
+    height: phoneList[selectedPhone].height * 4,
   };
-
 
   return (
     <>
-    <MobileTemplate style={phoneStyle}>
-      {/* <SignIn/> */}
-      <MainPage/>
-    </MobileTemplate>
-    <select value={selectedPhone} onChange={handleChange}>
+      <MobileTemplate style={phoneStyle}>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <SignIn />}/>
+          <Route path="/main" element={ <MainPage />}/>           
+        </Routes>
+        </BrowserRouter>
+      </MobileTemplate>
+      <select value={selectedPhone} onChange={handleChange}>
         {phoneOptions}
-    </select>
+      </select>
     </>
   );
 };
