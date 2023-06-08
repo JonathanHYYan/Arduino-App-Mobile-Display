3529204e641a4c1095d1c20df0313d5088ac9fca
Jonathan Yan <jonathanyan@Jonathans-MacBook-Pro.local>
2023-06-08 11:01:11 +0100

    [SDCWA-9] Create a dummy data set to generate dummy icons with

---
+++ src/Assets/data.js
@@ -0,0 +1,4 @@
+export const data = [
+    {icon: 'kitchen', toggle1: 'test', bool1: true, toggle2: 'test2', bool2: false},
+    {icon: 'bed', toggle1: 'test', bool1: true, toggle2: 'test2', bool2: false},
+]
--- src/Components/Main/MainPage.js
+++ src/Components/Main/MainPage.js
@@ -8,21 +8,31 @@ import {
   Nav,
   Plus,
 } from "./MainPageStyles";
+import { data } from "../../Assets/data";
 
 const MainPage = () => {
   // A data set needs to be imported or fetched from firebase on what applicances is available
   // Based on the data the applicance icon should show in the nav and the options in controls
   // lighter shade of orange #F5B181
 
+  const icons = data.map((obj) => {
+    return (
+      <Icon key={obj.icon}>
+        {obj.icon === "kitchen" ? (
+          <Kitchen />
+        ) : obj.icon === "bed" ? (
+          <Bed />
+        ) : (
+          ""
+        )}
+      </Icon>
+    );
+  });
+
   return (
     <DefaultPage>
       <Nav>
-        <Icon>
-          <Kitchen />
-        </Icon>
-        <Icon>
-          <Bed />
-        </Icon>
+        {icons}
         <AddIcon>
           <Plus />
         </AddIcon>
