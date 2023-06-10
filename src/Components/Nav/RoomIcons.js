3a3809c507374cab6e283594b706df4d4bd3c9fe
Jonathan Yan <jonathanyan@Jonathans-MacBook-Pro.local>
2023-06-10 16:43:12 +0100

    [SDCWA-22] cleaned up code

--- src/Components/Nav/RoomIcons.js
+++ src/Components/Nav/RoomIcons.js
@@ -2,21 +2,21 @@ import { Kitchen, Bed, Icon, Plus, AddIcon, Rooms } from "./RoomiconsStyles";
 import { data } from "../../Assets/data";
 
 const RoomIcons = () => {
-    const rooms = data.map((obj) => {
-        const roomSelect =
-          obj.room === "kitchen" ? <Kitchen /> : obj.room === "bed" ? <Bed /> : "";
-    
-        return <Icon key={obj.room}>{roomSelect}</Icon>;
-    });
+  const rooms = data.map((obj) => {
+    const roomSelect =
+      obj.room === "kitchen" ? <Kitchen /> : obj.room === "bed" ? <Bed /> : "";
 
-    return(
-        <Rooms>
-        {rooms}
-        <AddIcon>
-            <Plus/>
-        </AddIcon>
-        </Rooms>
-    )
+    return <Icon key={obj.room}>{roomSelect}</Icon>;
+  });
+
+  return (
+    <Rooms>
+      {rooms}
+      <AddIcon>
+        <Plus />
+      </AddIcon>
+    </Rooms>
+  );
 };
 
 export default RoomIcons;
