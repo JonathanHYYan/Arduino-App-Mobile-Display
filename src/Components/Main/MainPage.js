449dd07a56055a8b673fd81c5c2d92e55da582dc
Jonathan Yan <jonathanyan@Jonathans-MacBook-Pro.local>
2023-06-10 18:42:03 +0100

    [SDCWA-23] Refeactored controls into a seperate component

--- src/Components/Main/MainPage.js
+++ src/Components/Main/MainPage.js
@@ -1,41 +1,18 @@
 import {
-  AddControl,
-  Appliance,
-  Appliances,
   DefaultPage,
-  Plus,
 } from "./MainPageStyles";
-import { data } from "../../Assets/data";
-import Toggle from "../Utlity/Toggle";
-import RoomIcons from "../Nav/RoomIcons";
+import RoomIcons from "../Rooms/RoomIcons";
+import ApplianceControls from "../Appliance/ApplianceControls";
 
 const MainPage = () => {
   // A data set needs to be imported or fetched from firebase on what applicances is available
   // Based on the data the applicance icon should show in the nav and the options in controls
   // lighter shade of orange #F5B181
 
-  const appliances = data.map((obj) => {
-    const controls = obj.appliance.names.map((name, index) => {
-      return (
-        <Appliance key={name}>
-          <p>{name}</p>
-          <Toggle state={obj.appliance.boolean[index]} />
-        </Appliance>
-      );
-    });
-    return <>{controls}</>;
-  });
-
   return (
     <DefaultPage>
       <RoomIcons />
-      <Appliances>
-        <h2>Applicances</h2>
-        {appliances}
-        <AddControl>
-          <Plus />
-        </AddControl>
-      </Appliances>
+      <ApplianceControls />
     </DefaultPage>
   );
 };
