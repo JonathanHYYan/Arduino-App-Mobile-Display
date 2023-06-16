2f472d918e73449da26ff645179cc7bab952d77c
Jonathan Yan <jonathanyan@Jonathans-MacBook-Pro.local>
2023-06-16 22:12:44 +0100

    [SDCWA-17] Added feature to add a new room icon

---
+++ src/Components/Appliance/NewIcon.js
@@ -0,0 +1,47 @@
+import { Grid, GridIcon } from "./NewIconstyles";
+import { Bed, Kitchen } from "../Rooms/RoomiconsStyles";
+
+const NewIcon = ({updateRooms}) => {
+  const newRooms = (room) => {
+    updateRooms(room);
+  }
+
+  const RoomIcons = [
+    <GridIcon key="kitchen1" selected={false} onClick={() => newRooms("kitchen")}>
+      <Kitchen />
+    </GridIcon>,
+    <GridIcon key="bed2" selected={false} onClick={() => newRooms('bed')}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed3" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed4" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed5" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed6" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed7" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed8" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+    <GridIcon key="bed9" selected={false} onClick={() => newRooms("bed")}>
+      <Bed />
+    </GridIcon>,
+  ];
+
+  return (
+    <>
+      <h3>Select Icon For New Room: </h3>
+      <Grid>{RoomIcons}</Grid>
+    </>
+  );
+};
+
+export default NewIcon;
