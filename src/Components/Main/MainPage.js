e79d9ca9e440db7974d06e8bf152240c1c32c336
Jonathan Yan <jonathan.yan10@googlemail.com>
2023-06-23 15:38:18 +0100

    [SDCWA-27] Fix room selection bug

--- src/Components/Main/MainPage.js
+++ src/Components/Main/MainPage.js
@@ -1,21 +1,20 @@
 import { DefaultPage } from "./MainPageStyles";
 import RoomIcons from "../Rooms/RoomIcons";
 import ApplianceControls from "../Appliance/ApplianceControls";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { data } from "../../Assets/data";
 
 const MainPage = () => {
   // A data set needs to be imported or fetched from firebase on what applicances is available
-  const isMounted = useRef(false);
   const [database, setDataBase] = useState(data);
   const [roomSelected, setRoomSelected] = useState([true, false]);
 
   useEffect(() => {
-    console.log(roomSelected);
-  }, [database]);
+    // console.log(roomSelected);
+  }, [roomSelected]);
 
   const selectRoom = (index) => {
-    const updateRooms = roomSelected.map((i) => {
+    const updateRooms = roomSelected.map((room, i) => {
       if (index != null) {
         return i === index ? true : false;
       } else {
