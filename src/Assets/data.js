da90d6c0d5c221f6523dc7b1c6ca127e8dcd39e9
Jonathan Yan <jonathan.yan10@googlemail.com>
2023-07-17 17:53:40 +0100

    [SDCWA-40] Refeactored data set format

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -1,51 +1,65 @@
 export const data = [
   {
+    id: "000",
     room: "kitchen",
-    appliance: {
-      names: ["roomba", "lights"],
-      boolean: [true, false],
-    },
-    settings: [
+    appliance: [
       {
-        days: [false, true, false, false, false, false, false],
-        times: [
-          { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
-          { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
-        ],
-        vibrate: true,
+        id: "000",
+        name: "lights",
+        switchOn: true,
+        settings: {
+          days: [false, true, false, false, false, false, false],
+          times: [
+            { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
+            { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
+          ],
+          vibrate: true,
+        },
       },
       {
-        days: [false, true, false, false, false, false, false],
-        times: [
-          { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
-          { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
-        ],
-        vibrate: false,
+        id: "001",
+        name: "roomba",
+        switchOn: false,
+        settings: {
+          days: [false, true, false, false, false, false, false],
+          times: [
+            { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
+            { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
+          ],
+          vibrate: false,
+        },
       },
     ],
   },
   {
+    id: "001",
     room: "bed",
-    appliance: {
-      names: ["test11", "test22"],
-      boolean: [false, true],
-    },
-    settings: [
+    appliance: [
       {
-        days: [false, true, false, false, false, false, false],
-        times: [
-          { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
-          { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
-        ],
-        vibrate: false,
+        id: "000",
+        name: "lights",
+        switchOn: false,
+        settings: {
+          days: [false, true, false, false, false, false, false],
+          times: [
+            { startHr: 9, startMin: 0, endHr: 11, endMin: 0 },
+            { startHr: 18, startMin: 20, endHr: 19, endMin: 30 },
+          ],
+          vibrate: false,
+        },
       },
       {
-        days: [false, true, false, true, false, true, false],
-        times: [
-          { startHr: 2, startMin: 0, endHr: 4, endMin: 0 },
-          { startHr: 18, startMin: 8, endHr: 19, endMin: 7 },
-        ],
-        vibrate: true,
+        id: "001",
+        name: "kettle",
+        switchOn: true,
+        settings: {
+          days: [false, true, false, true, false, true, false],
+          times: [
+            { startHr: 2, startMin: 0, endHr: 4, endMin: 0 },
+            { startHr: 18, startMin: 8, endHr: 19, endMin: 7 },
+          ],
+          vibrate: true,
+        },
       },
     ],
   },
